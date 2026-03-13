d5bb00b4a93e15534ecf039869248f3afe939426
bsp456 <bhabani.panda2005@gmail.com>
2026-03-14 02:45:51 +0530

    prepare deployment

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ import mysql.connector
 def get_cursor(dictionary=False):
 
     db = mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database="beatmatch",
-        port=int(os.getenv("MYSQLPORT", 3306))
-    )
+    host=os.getenv("MYSQLHOST"),
+    user=os.getenv("MYSQLUSER"),
+    password=os.getenv("MYSQLPASSWORD"),
+    database="beatmatch",
+    port=int(os.getenv("MYSQLPORT", 3306))
+)
 
     cursor = db.cursor(dictionary=dictionary)
 
